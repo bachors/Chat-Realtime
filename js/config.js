b48c6fcac73241237a63b82f3fc19aef85b42e61
Ican Bachors <bachor.can@gmail.com>
2017-05-02 00:21:27 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,14 @@
-// Replace with: your_firebase_name.firebaseio.com
-var dbRef = new Firebase("https://REPLACE.firebaseio.com/"),
-	apis = 'api.php'; // MySQL API
-	
+// MySQL API
+var apis = 'api.php'; 
+
+// Replace with: your firebase account
+var config = {
+    apiKey: "AIzaSyDfKpgAUCOja3z-tc0yHOqzOCEGo0seJAQ",
+    databaseURL: "https://chatws-40480.firebaseio.com"
+};
+firebase.initializeApp(config);
+
 // create firebase child
-var messageRef = dbRef.child('message'),
+var dbRef = firebase.database().ref(),
+	messageRef = dbRef.child('message'),
 	userRef = dbRef.child('user');
