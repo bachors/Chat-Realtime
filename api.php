ae05bc48c9ad2a39832f878d130cb51cf8b6e3a7
Ican Bachors <bachor.can@gmail.com>
2017-11-29 20:50:34 +0700

    upload image

--- api.php
+++ api.php
@@ -4,7 +4,7 @@ session_start();
 		
 include_once('php/config.php');
 include_once('php/chat_realtime.php');
-$chat = new Chat_realtime($name, $host, $username, $password);
+$chat = new Chat_realtime($name, $host, $username, $password, $imageDir);
 		
 $data = array();
 
@@ -34,8 +34,29 @@ if(isset($_POST) and $_SERVER['REQUEST_METHOD'] == "POST"){
 		}else if($_POST['data'] == 'user'){
 			$data = $chat->get_user($_SESSION['user']);
 		}else if($_POST['data'] == 'send'){
-			if(isset($_SESSION['user']) && !empty($_POST['ke']) && !empty($_POST['message']) && !empty($_POST['date']) && !empty($_POST['avatar']) && !empty($_POST['tipe'])){
-				$data = $chat->send_message($_SESSION['user'], $_POST['ke'], $_POST['message'], $_POST['date'], $_POST['avatar'], $_POST['tipe']);
+			if(isset($_SESSION['user']) && !empty($_POST['ke']) && !empty($_POST['date']) && !empty($_POST['avatar']) && !empty($_POST['tipe']) && isset($_POST['message']) && isset($_POST['images'])){
+				$images = json_decode($_POST['images']);
+				if(!empty($_POST['message']) && count($images) < 1){
+					$data = $chat->send_message($_SESSION['user'], $_POST['ke'], $_POST['message'], "", $_POST['date'], $_POST['avatar'], $_POST['tipe']);
+				}else if(!empty($_POST['message']) && count($images) > 0){
+					$h = 0;
+					foreach($images as $image){
+						$n = $chat->arrayToBinaryString($image->binary);
+						$chat->createImg($n, $image->name, 'image/png');
+						if($h == 0){
+							$data = $chat->send_message($_SESSION['user'], $_POST['ke'], $_POST['message'], $image->name, $_POST['date'], $_POST['avatar'], $_POST['tipe']);
+						}else{	
+							$data = $chat->send_message($_SESSION['user'], $_POST['ke'], "", $image->name, $_POST['date'], $_POST['avatar'], $_POST['tipe']);
+						}
+						$h++;
+					}
+				}else if(empty($_POST['message']) && count($images) > 0){
+					foreach($images as $image){
+						$n = $chat->arrayToBinaryString($image->binary);
+						$chat->createImg($n, $image->name, 'image/png');
+						$data = $chat->send_message($_SESSION['user'], $_POST['ke'], "", $image->name, $_POST['date'], $_POST['avatar'], $_POST['tipe']);
+					}
+				}
 			}
 		}else if($_POST['data'] == 'logout'){
 			$data = $chat->user_logout($_SESSION['user']);
